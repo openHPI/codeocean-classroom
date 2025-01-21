openHPI/codeharbour

test(nbp): add request specs
Drop corresponding controller spec while maintaining full test coverage

Part of XI-6523

spec/requests/users/nbp_wallet/finalize_spec.rb
@@ -1,0 +1,218 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Account: NBPWallet: Finalize', type: :request do
+  subject(:finalize_request) { get '/users/nbp_wallet/finalize' }
+
+  let(:uid) { 'example-uid' }
+  let(:session_params) { {saml_uid: uid, omniauth_provider: 'nbp'} }
+
+  before do
+    set_session(session_params)
+  end
+
+  context 'without any errors' do
+    let(:relationship) do
+      instance_double(Enmeshed::Relationship,
+        accept!: true,
+        userdata: {
+          email: 'john.oliver@example103.org',
+          first_name: 'john',
+          last_name: 'oliver',
+          status_group: 'learner',
+        })
+    end
+
+    before do
+      allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+      allow(relationship).to receive(:peer).and_return('id1EvvJ68x6wdHBwYrFTR31XtALHko9fnbyp')
+    end
+
+    it 'creates a user' do
+      expect { finalize_request }.to change(User, :count).by(1)
+    end
+
+    it 'creates two UserIdentities' do
+      expect { finalize_request }.to change(UserIdentity, :count).by(2)
+    end
+
+    it 'accepts the Relationship' do
+      expect(relationship).to receive(:accept!)
+      finalize_request
+    end
+
+    it 'sends a confirmation mail' do
+      expect { finalize_request }.to change(ActionMailer::Base.deliveries, :count).by(1)
+    end
+
+    it 'does not create a confirmed user' do
+      finalize_request
+      expect(User.order(:created_at).last).not_to be_confirmed
+    end
+
+    it 'asks the user to verify the email address' do
+      finalize_request
+      expect(response).to redirect_to home_index_path
+      expect(flash[:notice]).to include I18n.t('devise.registrations.signed_up_but_unconfirmed')
+    end
+  end
+
+  context 'with errors' do
+    shared_examples 'a handled erroneous request' do |error_message|
+      it 'does not create a user' do
+        expect { finalize_request }.not_to change(User, :count)
+      end
+
+      it 'creates no UserIdentities' do
+        expect { finalize_request }.not_to change(UserIdentity, :count)
+      end
+
+      it 'redirects to the connect page' do
+        finalize_request
+        expect(response).to redirect_to nbp_wallet_connect_users_path
+      end
+
+      it 'displays an error message' do
+        finalize_request
+        expect(flash[:alert]).to eq error_message
+      end
+
+      it 'does not send a confirmation mail' do
+        expect { finalize_request }.not_to change(ActionMailer::Base, :deliveries)
+      end
+    end
+
+    shared_examples 'a documented erroneous request' do |error|
+      it 'passes the error reason to Sentry' do
+        expect(Sentry).to receive(:capture_exception) do |e|
+          expect(e).to be_a error
+        end
+        finalize_request
+      end
+    end
+
+    context 'without the session' do
+      let(:session_params) { {} }
+
+      before do
+        finalize_request
+      end
+
+      it_behaves_like 'an unauthorized request'
+    end
+
+    context 'when the connector is down' do
+      before { allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_raise(Faraday::ConnectionFailed) }
+
+      it 'redirects to the connect page' do
+        finalize_request
+        expect(response).to redirect_to(nbp_wallet_connect_users_path)
+      end
+    end
+
+    context 'when an attribute is missing' do
+      # `Enmeshed::ConnectorError` is unknown until 'lib/enmeshed/connector.rb' is loaded, because it's defined there
+      require 'enmeshed/connector'
+
+      let(:relationship) do
+        instance_double(Enmeshed::Relationship, accept!: false, reject!: true)
+      end
+
+      before do
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+        allow(relationship).to receive(:userdata).and_raise(Enmeshed::ConnectorError, 'EMailAddress must not be empty')
+      end
+
+      it_behaves_like 'a handled erroneous request', I18n.t('common.errors.generic')
+      it_behaves_like 'a documented erroneous request', Enmeshed::ConnectorError
+    end
+
+    context 'with an invalid status group' do
+      let(:relationship) do
+        instance_double(Enmeshed::Relationship, 
+          reject!: true,
+          userdata: {
+            email: 'john.oliver@example103.org',
+            first_name: 'john',
+            last_name: 'oliver',
+            status_group: nil,
+          })
+      end
+
+      before do
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+      end
+
+      it_behaves_like 'a handled erroneous request', 'Could not create User: Unknown role. Please select either ' \
+                                                     '"Teacher" or "Student" as your role.'
+    end
+
+    context 'when the User cannot be saved' do
+      let(:relationship) do
+        instance_double(Enmeshed::Relationship,
+          reject!: true,
+          userdata: {
+            email: 'john.oliver@example103.org',
+            first_name: 'john',
+            last_name: 'oliver',
+            status_group: 'learner',
+          })
+      end
+
+      before do
+        create(:user, email: relationship.userdata[:email])
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+        allow(relationship).to receive(:peer).and_return('id1EvvJ68x6wdHBwYrFTR31XtALHko9fnbyp')
+      end
+
+      it_behaves_like 'a handled erroneous request', 'Could not create User: Email has already been taken'
+    end
+
+    context 'when the RelationshipChange cannot be accepted' do
+      let(:relationship) do
+        instance_double(Enmeshed::Relationship,
+          accept!: false,
+          reject!: true,
+          userdata: {
+            email: 'john.oliver@example103.org',
+            first_name: 'john',
+            last_name: 'oliver',
+            status_group: 'learner',
+          })
+      end
+
+      before do
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+        allow(relationship).to receive(:peer).and_return('id1EvvJ68x6wdHBwYrFTR31XtALHko9fnbyp')
+      end
+
+      it_behaves_like 'a handled erroneous request', I18n.t('common.errors.generic')
+
+      it 'rejects the Relationship' do
+        expect(relationship).to receive(:reject!)
+        finalize_request
+      end
+
+      context 'when the RelationshipChange cannot be rejected either' do
+        before { allow(relationship).to receive(:reject!).and_raise(Faraday::ConnectionFailed) }
+
+        it_behaves_like 'a handled erroneous request', I18n.t('common.errors.generic')
+        it_behaves_like 'a documented erroneous request', Faraday::ConnectionFailed
+
+        it 'does not create a user' do
+          expect { finalize_request }.not_to change(User, :count)
+        end
+
+        it 'creates no UserIdentities' do
+          expect { finalize_request }.not_to change(UserIdentity, :count)
+        end
+
+        it 'does not try to reject the Relationship again' do
+          expect(relationship).to receive(:reject!).once
+          finalize_request
+        end
+      end
+    end
+  end
+end

spec/requests/users/nbp_wallet/qr_code_spec.rb
@@ -1,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Account: NBPWallet: QRCode', type: :request do
+  subject(:qr_code_request) { get '/users/nbp_wallet/qr_code', params: {truncated_reference:} }
+
+  let(:uid) { 'example-uid' }
+  let(:session_params) { {saml_uid: uid, omniauth_provider: 'nbp'} }
+
+  let(:truncated_reference) { 'example_truncated_reference' }
+  let(:qr_code) { RQRCode::QRCode.new("nmshd://tr##{truncated_reference}").as_png(border_modules: 0) }
+  let(:relationship_template) do
+    instance_double(Enmeshed::RelationshipTemplate, qr_code:)
+  end
+
+  before do
+    set_session(session_params)
+    allow(Enmeshed::RelationshipTemplate).to receive(:new).with(truncated_reference:).and_return(relationship_template)
+  end
+
+  it 'returns a png image' do
+    qr_code_request
+    expect(response.content_type).to eq 'image/png'
+  end
+
+  it 'initializes a RelationshipTemplate' do
+    expect(Enmeshed::RelationshipTemplate).to receive(:new)
+    qr_code_request
+  end
+
+  it 'sends the qr code' do
+    qr_code_request
+    expect(response.body).to eq qr_code.to_s
+  end
+
+  context 'without the session' do
+    let(:session_params) { {} }
+
+    before do
+      qr_code_request
+    end
+
+    it_behaves_like 'an unauthorized request'
+  end
+end

spec/requests/users/nbp_wallet/relationship_status_spec.rb
@@ -1,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Account: NBPWallet: RelationshipStatus', type: :request do
+  subject(:relationship_status_request) { get '/users/nbp_wallet/relationship_status' }
+
+  let(:uid) { 'example_uid' }
+  let(:session_params) { {saml_uid: uid, omniauth_provider: 'nbp'} }
+
+  before do
+    set_session(session_params)
+  end
+
+  context 'without errors' do
+    context 'with a Relationship' do
+      before { allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(Enmeshed::Relationship) }
+
+      it 'returns a json with the ready status' do
+        relationship_status_request
+        expect(response.parsed_body['status']).to eq 'ready'
+      end
+    end
+
+    context 'without a Relationship' do
+      before do
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return([])
+      end
+
+      it 'returns a json with the waiting status' do
+        relationship_status_request
+        expect(response.parsed_body['status']).to eq 'waiting'
+      end
+    end
+  end
+
+  context 'with errors' do
+    # `Enmeshed::ConnectorError` is unknown until 'lib/enmeshed/connector.rb' is loaded, because it's defined there
+    require 'enmeshed/connector'
+
+    context 'without the session' do
+      let(:session_params) { {} }
+
+      before do
+        relationship_status_request
+      end
+
+      it_behaves_like 'an unauthorized request'
+    end
+
+    context 'when the connector is down' do
+      before { allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_raise(Faraday::ConnectionFailed) }
+
+      it 'redirects to the connect page' do
+        relationship_status_request
+        expect(response).to redirect_to(nbp_wallet_connect_users_path)
+      end
+    end
+
+    context 'with an error when parsing the connector response' do
+      before { allow(Enmeshed::Connector).to receive(:pending_relationships).and_raise(Enmeshed::ConnectorError) }
+
+      it 'redirects to the connect page' do
+        relationship_status_request
+        expect(response).to redirect_to(nbp_wallet_connect_users_path)
+      end
+    end
+  end
+end

openHPI/codeocean-classroom

spec/requests/users/nbp_wallet/finalize_spec.rb
@@ -1,0 +1,218 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Users: NBPWallet: Finalize', type: :request do
+  subject(:finalize_request) { get '/users/nbp_wallet/finalize' }
+
+  let(:uid) { 'example-uid' }
+  let(:session_params) { {saml_uid: uid, omniauth_provider: 'nbp'} }
+
+  before do
+    set_session(session_params)
+  end
+
+  context 'without any errors' do
+    let(:relationship) do
+      instance_double(Enmeshed::Relationship,
+        accept!: true,
+        userdata: {
+          email: 'john.oliver@example103.org',
+          first_name: 'john',
+          last_name: 'oliver',
+          status_group: 'learner',
+        })
+    end
+
+    before do
+      allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+      allow(relationship).to receive(:peer).and_return('id1EvvJ68x6wdHBwYrFTR31XtALHko9fnbyp')
+    end
+
+    it 'creates a user' do
+      expect { finalize_request }.to change(User, :count).by(1)
+    end
+
+    it 'creates two UserIdentities' do
+      expect { finalize_request }.to change(UserIdentity, :count).by(2)
+    end
+
+    it 'accepts the Relationship' do
+      expect(relationship).to receive(:accept!)
+      finalize_request
+    end
+
+    it 'sends a confirmation mail' do
+      expect { finalize_request }.to change(ActionMailer::Base.deliveries, :count).by(1)
+    end
+
+    it 'does not create a confirmed user' do
+      finalize_request
+      expect(User.order(:created_at).last).not_to be_confirmed
+    end
+
+    it 'asks the user to verify the email address' do
+      finalize_request
+      expect(response).to redirect_to home_index_path
+      expect(flash[:notice]).to include I18n.t('devise.registrations.signed_up_but_unconfirmed')
+    end
+  end
+
+  context 'with errors' do
+    shared_examples 'a handled erroneous request' do |error_message|
+      it 'does not create a user' do
+        expect { finalize_request }.not_to change(User, :count)
+      end
+
+      it 'creates no UserIdentities' do
+        expect { finalize_request }.not_to change(UserIdentity, :count)
+      end
+
+      it 'redirects to the connect page' do
+        finalize_request
+        expect(response).to redirect_to nbp_wallet_connect_users_path
+      end
+
+      it 'displays an error message' do
+        finalize_request
+        expect(flash[:alert]).to eq error_message
+      end
+
+      it 'does not send a confirmation mail' do
+        expect { finalize_request }.not_to change(ActionMailer::Base, :deliveries)
+      end
+    end
+
+    shared_examples 'a documented erroneous request' do |error|
+      it 'passes the error reason to Sentry' do
+        expect(Sentry).to receive(:capture_exception) do |e|
+          expect(e).to be_a error
+        end
+        finalize_request
+      end
+    end
+
+    context 'without the session' do
+      let(:session_params) { {} }
+
+      before do
+        finalize_request
+      end
+
+      it_behaves_like 'an unauthorized request'
+    end
+
+    context 'when the connector is down' do
+      before { allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_raise(Faraday::ConnectionFailed) }
+
+      it 'redirects to the connect page' do
+        finalize_request
+        expect(response).to redirect_to(nbp_wallet_connect_users_path)
+      end
+    end
+
+    context 'when an attribute is missing' do
+      # `Enmeshed::ConnectorError` is unknown until 'lib/enmeshed/connector.rb' is loaded, because it's defined there
+      require 'enmeshed/connector'
+
+      let(:relationship) do
+        instance_double(Enmeshed::Relationship, accept!: false, reject!: true)
+      end
+
+      before do
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+        allow(relationship).to receive(:userdata).and_raise(Enmeshed::ConnectorError, 'EMailAddress must not be empty')
+      end
+
+      it_behaves_like 'a handled erroneous request', I18n.t('common.errors.generic')
+      it_behaves_like 'a documented erroneous request', Enmeshed::ConnectorError
+    end
+
+    context 'with an invalid status group' do
+      let(:relationship) do
+        instance_double(Enmeshed::Relationship,
+          reject!: true,
+          userdata: {
+            email: 'john.oliver@example103.org',
+            first_name: 'john',
+            last_name: 'oliver',
+            status_group: nil,
+          })
+      end
+
+      before do
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+      end
+
+      it_behaves_like 'a handled erroneous request', 'Could not create User: Unknown role. Please select either ' \
+                                                     '"Teacher" or "Student" as your role.'
+    end
+
+    context 'when the User cannot be saved' do
+      let(:relationship) do
+        instance_double(Enmeshed::Relationship,
+          reject!: true,
+          userdata: {
+            email: 'john.oliver@example103.org',
+            first_name: 'john',
+            last_name: 'oliver',
+            status_group: 'learner',
+          })
+      end
+
+      before do
+        create(:user, email: relationship.userdata[:email])
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+        allow(relationship).to receive(:peer).and_return('id1EvvJ68x6wdHBwYrFTR31XtALHko9fnbyp')
+      end
+
+      it_behaves_like 'a handled erroneous request', 'Could not create User: Email has already been taken'
+    end
+
+    context 'when the RelationshipChange cannot be accepted' do
+      let(:relationship) do
+        instance_double(Enmeshed::Relationship,
+          accept!: false,
+          reject!: true,
+          userdata: {
+            email: 'john.oliver@example103.org',
+            first_name: 'john',
+            last_name: 'oliver',
+            status_group: 'learner',
+          })
+      end
+
+      before do
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(relationship)
+        allow(relationship).to receive(:peer).and_return('id1EvvJ68x6wdHBwYrFTR31XtALHko9fnbyp')
+      end
+
+      it_behaves_like 'a handled erroneous request', I18n.t('common.errors.generic')
+
+      it 'rejects the Relationship' do
+        expect(relationship).to receive(:reject!)
+        finalize_request
+      end
+
+      context 'when the RelationshipChange cannot be rejected either' do
+        before { allow(relationship).to receive(:reject!).and_raise(Faraday::ConnectionFailed) }
+
+        it_behaves_like 'a handled erroneous request', I18n.t('common.errors.generic')
+        it_behaves_like 'a documented erroneous request', Faraday::ConnectionFailed
+
+        it 'does not create a user' do
+          expect { finalize_request }.not_to change(User, :count)
+        end
+
+        it 'creates no UserIdentities' do
+          expect { finalize_request }.not_to change(UserIdentity, :count)
+        end
+
+        it 'does not try to reject the Relationship again' do
+          expect(relationship).to receive(:reject!).once
+          finalize_request
+        end
+      end
+    end
+  end
+end

spec/requests/users/nbp_wallet/qr_code_spec.rb
@@ -1,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Users: NBPWallet: QRCode', type: :request do
+  subject(:qr_code_request) { get '/users/nbp_wallet/qr_code', params: {truncated_reference:} }
+
+  let(:uid) { 'example-uid' }
+  let(:session_params) { {saml_uid: uid, omniauth_provider: 'nbp'} }
+
+  let(:truncated_reference) { 'example_truncated_reference' }
+  let(:qr_code) { RQRCode::QRCode.new("nmshd://tr##{truncated_reference}").as_png(border_modules: 0) }
+  let(:relationship_template) do
+    instance_double(Enmeshed::RelationshipTemplate, qr_code:)
+  end
+
+  before do
+    set_session(session_params)
+    allow(Enmeshed::RelationshipTemplate).to receive(:new).with(truncated_reference:).and_return(relationship_template)
+  end
+
+  it 'returns a png image' do
+    qr_code_request
+    expect(response.content_type).to eq 'image/png'
+  end
+
+  it 'initializes a RelationshipTemplate' do
+    expect(Enmeshed::RelationshipTemplate).to receive(:new)
+    qr_code_request
+  end
+
+  it 'sends the qr code' do
+    qr_code_request
+    expect(response.body).to eq qr_code.to_s
+  end
+
+  context 'without the session' do
+    let(:session_params) { {} }
+
+    before do
+      qr_code_request
+    end
+
+    it_behaves_like 'an unauthorized request'
+  end
+end

spec/requests/users/nbp_wallet/relationship_status_spec.rb
@@ -1,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Users: NBPWallet: RelationshipStatus', type: :request do
+  subject(:relationship_status_request) { get '/users/nbp_wallet/relationship_status' }
+
+  let(:uid) { 'example-uid' }
+  let(:session_params) { {saml_uid: uid, omniauth_provider: 'nbp'} }
+
+  before do
+    set_session(session_params)
+  end
+
+  context 'without errors' do
+    context 'with a Relationship' do
+      before { allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return(Enmeshed::Relationship) }
+
+      it 'returns a json with the ready status' do
+        relationship_status_request
+        expect(response.parsed_body['status']).to eq 'ready'
+      end
+    end
+
+    context 'without a Relationship' do
+      before do
+        allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_return([])
+      end
+
+      it 'returns a json with the waiting status' do
+        relationship_status_request
+        expect(response.parsed_body['status']).to eq 'waiting'
+      end
+    end
+  end
+
+  context 'with errors' do
+    # `Enmeshed::ConnectorError` is unknown until 'lib/enmeshed/connector.rb' is loaded, because it's defined there
+    require 'enmeshed/connector'
+
+    context 'without the session' do
+      let(:session_params) { {} }
+
+      before do
+        relationship_status_request
+      end
+
+      it_behaves_like 'an unauthorized request'
+    end
+
+    context 'when the connector is down' do
+      before { allow(Enmeshed::Relationship).to receive(:pending_for).with(uid).and_raise(Faraday::ConnectionFailed) }
+
+      it 'redirects to the connect page' do
+        relationship_status_request
+        expect(response).to redirect_to(nbp_wallet_connect_users_path)
+      end
+    end
+
+    context 'with an error when parsing the connector response' do
+      before { allow(Enmeshed::Connector).to receive(:pending_relationships).and_raise(Enmeshed::ConnectorError) }
+
+      it 'redirects to the connect page' do
+        relationship_status_request
+        expect(response).to redirect_to(nbp_wallet_connect_users_path)
+      end
+    end
+  end
+end